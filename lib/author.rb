14ab51f639c2f45c3f7d8e3ad678f40204bb9e12
jbaxter6 <jbaxter6@radford.edu>
2020-06-07 20:20:18 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+
+    # attr_reader :name
+
+    # def initialize (author_name)
+    #     @name = author_name
+    # end
+
+    def name=(author_name)
+        @name = author_name
+    end
+
+    def name 
+        @name
+    end
+
+end
